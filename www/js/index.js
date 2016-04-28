7b2dfc5c30c320b5467220221da3e91202e55cca
Matt Ng <Matt.Ng@reece.com.au>
2016-04-28 13:59:37 +1000

    Add jquery

--- www/js/index.js
+++ www/js/index.js
@@ -35,23 +35,12 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
 
-        if(typeof (nfc) !== 'undefined') {alert('1');} else {alert('2');}
-
+        function failure(reason) {
+            navigator.notification.alert(reason, function() {}, "There was a problem");
+        }
 
         nfc.addNdefListener (
-            function (nfcEvent) {
-                var tag = nfcEvent.tag,
-                    ndefMessage = tag.ndefMessage;
-
-                // dump the raw json of the message
-                // note: real code will need to decode
-                // the payload from each record
-                alert(JSON.stringify(ndefMessage));
-
-                // assuming the first record in the message has
-                // a payload that can be converted to a string.
-                alert(nfc.bytesToString(ndefMessage[0].payload).substring(3));
-            },
+            app.onNdef,
             function () { // success callback
                 alert("Waiting for NDEF tag");
             },
@@ -60,38 +49,46 @@ var app = {
             }
         );
 
-        nfc.addMimeTypeListener('', app.onNfc,
-            function () { // success callback
-                alert("Waiting for NDEF tag");
-            },
-            function (error) { // error callback
-                alert("Error adding NDEF listener " + JSON.stringify(error));
-            } )
+        if (device.platform == "Android") {
 
-        nfc.addMimeTypeListener(
-            'text/pg',
-            app.onNfc,
-            function () { // success callback
-                alert("Waiting for NDEF tag");
-            },
-            function (error) { // error callback
-                alert("Error adding NDEF listener " + JSON.stringify(error));
-            }
-        );
+            // Android reads non-NDEF tag. BlackBerry and Windows don't.
+            nfc.addTagDiscoveredListener(
+                app.onNfc,
+                function() {
+                    console.log("Listening for non-NDEF tags.");
+                },
+                failure
+            );
+
+            // Android launches the app when tags with mime type text/pg are scanned
+            // because of an intent in AndroidManifest.xml.
+            // phonegap-nfc fires an ndef-mime event (as opposed to an ndef event)
+            // the code reuses the same onNfc handler
+            nfc.addMimeTypeListener(
+                'text/pg',
+                app.onNdef,
+                function() {
+                    console.log("Listening for NDEF mime tags with type text/pg.");
+                },
+                failure
+            );
+        }
 
     },
     onNfc: function (nfcEvent) {
 
-        var success = function() {
-            alert("Wrote data to tag");
-        };
-        var failure = function(reason) {
-            alert("NFC write failed " + reason);
-        };
+        var tag = nfcEvent.tag;
+        var ndefMessage = tag.ndefMessage;
+
+        alert('onNfc tag:' + tag + "; ndefMessage: " + ndefMessage);
+
+        navigator.notification.vibrate(100);
+    },
+    onNdef: function (nfcEvent) {
 
         var tag = nfcEvent.tag;
-
-        alert(JSON.stringify(tag));
+        var ndefMessage = tag.ndefMessage;
+        alert('onNfc tag:' + tag + "; ndefMessage: " + ndefMessage);
 
         navigator.notification.vibrate(100);
     },
